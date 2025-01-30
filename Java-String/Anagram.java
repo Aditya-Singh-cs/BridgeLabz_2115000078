5c0dd2a5c13ebfc51603c5abfd73aec3bb338ef0
Aditya Singh <134596699+Aditya-Singh-cs@users.noreply.github.com>
2025-01-30 13:43:29 +0530

    Update Anagram.java

--- Java-String/Anagram.java
+++ Java-String/Anagram.java
@@ -1,42 +1,59 @@
 import java.util.*;
-class Anagram{
+class Anagram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the first string: ");
-        String str1 = sc.nextLine();
-        
+        String str1 = sc.nextLine().toLowerCase();
         System.out.print("Enter the second string: ");
-        String str2 = sc.nextLine();
-        
-        // Call the method to check if the strings are anagrams
+        String str2 = sc.nextLine().toLowerCase();
         if (areAnagrams(str1, str2)) {
             System.out.println("The strings are anagrams.");
         } else {
             System.out.println("The strings are not anagrams.");
         }
-        sc.close();
     }
 
-    // Method to check if two strings are anagrams
     public static boolean areAnagrams(String str1, String str2) {
-        // Remove spaces and convert to lowercase for case insensitivity
-        str1 = str1.replaceAll("\\s", "").toLowerCase();
-        str2 = str2.replaceAll("\\s", "").toLowerCase();
-        
-        // Check if the lengths of the strings are the same
+        str1 = normalize(str1);
+        str2 = normalize(str2);
+
         if (str1.length() != str2.length()) {
             return false;
         }
-        
-        // Convert both strings to character arrays
+
         char[] charArray1 = str1.toCharArray();
         char[] charArray2 = str2.toCharArray();
-        
-        // Sort the character arrays
-        Arrays.sort(charArray1);
-        Arrays.sort(charArray2);
-        
-        // Compare the sorted arrays
-        return Arrays.equals(charArray1, charArray2);
+        manualSort(charArray1);
+        manualSort(charArray2);
+
+        for (int i = 0; i < charArray1.length; i++) {
+            if (charArray1[i] != charArray2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String normalize(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch != ' ') {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void manualSort(char[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    char temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 }
