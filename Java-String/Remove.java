7b2b6c56dfca56d57dd5cbec3137a192e2f8beb6
Aditya-Singh-cs <aditya76596@gmail.com>
2025-01-30 14:02:44 +0530

    Updated Remove.java

--- Java-String/Remove.java
+++ Java-String/Remove.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
-class Remove{
+import java.util.*;
+class Remove {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
         System.out.print("Enter character to remove: ");
         char charToRemove = sc.next().charAt(0);
-        
-        // Call the method to remove the specific character
+
         String modifiedString = removeCharacter(input, charToRemove);
         System.out.println("Modified String: " + modifiedString);
     }
 
-    // Method to remove all occurrences of a specific character from the string
     public static String removeCharacter(String str, char charToRemove) {
-        // Use String's replace method to remove all occurrences of the specified character
-        return str.replace(Character.toString(charToRemove), "");
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != charToRemove) {
+                result += str.charAt(i);
+            }
+        }
+        return result;
     }
 }
